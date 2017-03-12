0f5aaf985b3b72becc21e43f1444bd1de76a3be0
subond <yubc0321@gmail.com>
2017-03-12 19:36:18 +0800

    add thread

--- Notes/4_2.c
+++ Notes/4_2.c
@@ -15,17 +15,6 @@ int main() {
   pid = fork();
   if(pid == 0) {
     pthread_attr_init(&attr);
-    int scope;
-    if(pthread_attr_getscope(&attr, &scope) != 0) {
-      fprintf(stderr, "Unable to get scheduling scope\n");
-    } else {
-      if(scope == PTHREAD_SCOPE_PROCESS)
-        printf("PTHREAD_SCOPE_PROCESS\n");
-      else if(scope == PTHREAD_SCOPE_SYSTEM)
-        printf("PTHREAD_SCOPE_SYSTEM\n");
-      else
-        fprintf(stderr, "Illegal scope value\n");
-    }
     int id = pthread_create(&tid, &attr, runner, NULL);
     pthread_join(tid, NULL);
     printf("CHILD: value = %d\n", value);
